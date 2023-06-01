02294585d3da6f59109c5c69899d2c21bfbf6392
reoKaizaki <kaizaki.reo.22@shizuoka.ac.jp>
2023-06-01 12:02:29 +0900

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <p>Hello, world!</p>
+        <p>Hello,world!</p>
+        <p>こんにちは^-^</p>
       </main>
     </>
   );
