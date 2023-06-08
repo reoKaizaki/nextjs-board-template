db602fb7c10f6ff09490e8b8601c03e7014f67a2
reoKaizaki <kaizaki.reo.22@shizuoka.ac.jp>
2023-06-08 17:41:37 +0900

    投稿ボックス作成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import React, { useState, useEffect } from "react";
 
 export default function Home() {
+  const [data, setData] = useState("");
+  const [name, setName] = useState("");
+  const [message, setMessage] = useState("");
+  const post = async () => {
+    setData("名前: " + name + " / メッセージ: " + message);
+    setName("");
+    setMessage("");
+  };
   return (
     <>
       <Head>
@@ -11,8 +20,30 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main}`}>
-        <p>Hello,world!</p>
-        <p>こんにちは^-^</p>
+      <div>
+  <p>
+	名前:
+	<br />
+	<input
+	  type="text"
+	  value={name}
+	  onChange={(e) => setName(e.target.value)}
+	/>
+  </p>
+  <p>
+	内容:
+	<br />
+	<textarea
+	  value={message}
+	  onChange={(e) => setMessage(e.target.value)}
+	></textarea>
+  </p>
+  <p>
+	<button onClick={post}>投稿</button>
+  </p>
+</div>
+<hr />
+<div>{data}</div>
       </main>
     </>
   );
